b024769a4e4e8670e4c60b7de7892d6592b1d927
Nanthagopal <balajinantha68@gmail.com>
2022-12-11 17:40:11 +0530

    Adding Encapsulation Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly r = new RWOnly();
+        // Error : name has private access in com.driver.RWOnly
+        r.setName("Accio");
+        System.out.println(r.getName());
+    }
 }
